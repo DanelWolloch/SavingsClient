37a97b2b71eec4899601e070c2022c86d4083e5f
Danel Wolloch <dwolloch14@gmail.com>
2012-10-12 22:58:24 +0200

    Can add new save...

---
+++ SavingsClient/AddPageScript.js
@@ -0,0 +1,32 @@
+﻿function addSave() {
+    if (supports_html5_storage()) {
+        var numOfSavings = localStorage["numOfSavings"];
+
+        // Check if this is the first save
+        if (numOfSavings == null) {
+            numOfSavings = 0;
+        }
+
+        ++numOfSavings;
+
+        var type = $('#saveType').val();
+        var date = $('#saveDate').val();
+        var notes = $('#saveNotes').val();
+
+        localStorage["numOfSavings"] = numOfSavings;
+        localStorage['saveType' + numOfSavings] = type;
+        localStorage['saveDate' + numOfSavings] = date;
+        localStorage['saveNotes' + numOfSavings] = notes;
+
+    } else {
+        alert('מכשיר לא נתמך');
+    }
+}
+
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
--- SavingsClient/SavingScript.js
+++ SavingsClient/SavingScript.js
@@ -1,1 +1,35 @@
-﻿
+﻿function loadTable() {
+    if (supports_html5_storage()) {
+
+        var numOfSavings = localStorage["numOfSavings"];
+
+        if (numOfSavings != null) {
+
+            var type = localStorage['saveType' + numOfSavings];
+            var date = localStorage['saveDate' + numOfSavings];
+            var notes = localStorage['saveNotes' + numOfSavings];
+
+
+
+            var newRow = "<tr>" +
+                            "<td>" + numOfSavings + "</td>" +
+                            "<td>" + type + "</td>" +
+                            "<td>" + date + "</td>" +
+                            "<td>" + notes + "</td>"
+                        "</tr>";
+
+            $('.MyTable').append(newRow);
+        }
+
+    } else {
+        alert('מכשיר לא נתמך');
+    }
+}
+
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
