c52cce39562cb1731b9f75429d70085617efddfe
Danel Wolloch <dwolloch14@gmail.com>
2012-10-13 00:30:37 +0200

    Delete all button was added

--- SavingsClient/SavingScript.js
+++ SavingsClient/SavingScript.js
@@ -1,9 +1,17 @@
-﻿function loadTable() {
+﻿function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+function loadTable() {
     if (supports_html5_storage()) {
 
         var numOfSavings = localStorage["numOfSavings"];
 
-        if (numOfSavings != null) {
+        if (numOfSavings != null && isNaN(numOfSavings) && numOfSavings > 0) {
 
             var type = localStorage['saveType' + numOfSavings];
             var date = localStorage['saveDate' + numOfSavings];
@@ -16,7 +24,7 @@
                             "<td>" + type + "</td>" +
                             "<td>" + date + "</td>" +
                             "<td>" + notes + "</td>"
-                        "</tr>";
+            "</tr>";
 
             $('.MyTable').append(newRow);
         }
@@ -26,10 +34,14 @@
     }
 }
 
-function supports_html5_storage() {
-    try {
-        return 'localStorage' in window && window['localStorage'] !== null;
-    } catch (e) {
-        return false;
+function deleteAll() {
+    var numOfSavings = localStorage["numOfSavings"];
+
+    for (var i = 1; i < numOfSavings + 1; i++) {
+        localStorage.removeItem('saveType' + i);
+        localStorage.removeItem('saveDate' + i);
+        localStorage.removeItem('saveNotes' + i);
     }
+
+    localStorage["numOfSavings"] = 0;
 }
